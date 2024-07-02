d64f7a2cda49b253a99eca4b6fd6c48b28b9fb2a
tahfeezmizan <mizanahmed5316@gmail.com>
2024-07-03 01:46:34 +0600

    create responsive navbar

---
+++ client-site/src/page/Navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/main-logo.png'
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    const handleToggleOpen = () => {
+        setIsOpen(!isOpen)
+    }
+
+    return (
+        <nav className="relative  shadow">
+            <div className="container px-6 py-4 mx-auto lg:flex lg:justify-between lg:items-center">
+                <div className="flex items-center justify-between">
+                    <Link to="/" className="flex gap-3">
+                        <img className="w-auto h-6 sm:h-7" src={logo} alt="" />
+                    </Link>
+
+                    {/* <!-- Mobile menu button --> */}
+                    <div className="flex  lg:hidden">
+                        <button onClick={handleToggleOpen} type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+                            {
+                                !isOpen ? (<svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 8h16M4 16h16" />
+                                </svg>) : (<svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                </svg>)
+                            }
+                        </button>
+                    </div>
+                </div>
+
+                {/* <!-- Mobile Menu open: "block", Menu closed: "hidden" --> */}
+                <div className={`absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-base-100   lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center ${isOpen ? 'block' : 'hidden'}`}>
+
+                    <div className="flex flex-col lg:items-center lg:flex-row lg:mx-6">
+                        <Link to="/" className="my-2   transition-colors duration-300 transform hover:text-[#6A38C2]  md:mx-4 md:my-0">Home</Link>
+                        <Link to="/blogs" className="my-2  transition-colors duration-300 transform dark:text-gray-200 hover:text-[#6A38C2]  md:mx-4 md:my-0">Blogs</Link>
+                        <Link to="/all-jobs" className="my-2  transition-colors duration-300 transform dark:text-gray-200 hover:text-[#6A38C2]  md:mx-4 md:my-0">All Jobs</Link>
+
+                        <Link to="" className="my-2  transition-colors duration-300 transform lg:hidden dark:text-gray-200 hover:text-[#6A38C2]  md:mx-4 md:my-0">Profile</Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
