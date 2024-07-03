af68a684cf13743ed75a6820e3913b182b930681
tahfeezmizan <mizanahmed5316@gmail.com>
2024-07-03 11:54:56 +0600

    setup backend server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('My Portflio server is running')
+});
+
+app.listen(port, () => {
+    console.log(`Portflio is running port on ${port}`)
+})
