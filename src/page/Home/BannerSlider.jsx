952fb5da37e57c6e232edeb117e81e3abb079a02
tahfeezmizan <mizanahmed5316@gmail.com>
2025-02-20 22:05:00 +0600

    update title and improve styling; replace NavLink with anchor tag for contact section

--- src/page/Home/BannerSlider.jsx
+++ src/page/Home/BannerSlider.jsx
@@ -17,12 +17,12 @@ const BannerSlider = () => {
                                 <h1 className="sliderTitle pb-4 font-NotoSans md:text-[58px] font-bold z-50">I'm <span className='text-primaryTextColor'>Tahfeez Mizan </span>Mern Stack Developer</h1>
                                 <p className="pb-8 text-2xl text-textColor font-light font-Rubik pr-28 slider-Dec">I love to create things that people love. If you are looking for an ongoing development, don't hesitate to contact me!</p>
 
-                                <NavLink  className="flex items-center gap-5">
+                                <a href='#contact'  className="flex items-center gap-5">
                                     <Button className="NavButton mb-5 font-Rubik text-lg font-medium inline-flex items-center justify-center gap-2 rounded-lg w-40 h-10 border-2 py-6 bg-transparent text-primaryTextColor border-primaryTextColor hover:bg-primaryTextColor hover:text-white">
                                         <FaChevronDown />
                                         Hire Me
                                     </Button>
-                                </NavLink>
+                                </a>
                             </div>
                         </div>
 
